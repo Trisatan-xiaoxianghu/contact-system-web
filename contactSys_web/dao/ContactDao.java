effbd360c1ca1f7cf361e0c58803d4c9ddb667e9
Trisatan-xiaoxianghu <hxxjesse@163.com>
2017-06-30 11:31:48 +0200

    Signed-off-by: Trisatan-xiaoxianghu <hxxjesse@163.com>

---
+++ contactSys_web/dao/ContactDao.java
@@ -0,0 +1,14 @@
+﻿package contactSys_web.dao;
+
+import contactSys_web.entity.Contact;
+
+import java.util.List;
+
+
+public interface ContactDao {
+	public void addContact(Contact contact);//添加联系人
+	public void updateContact(Contact contact);//修改联系人
+	public void deleteContact(String id);//删除联系人
+	public List<Contact> findAll();  //查询所有联系人
+	public Contact findById(String id);//根据编号查询联系人
+}
---
+++ contactSys_web/servlet/DeleteContactServlet.java
@@ -0,0 +1,35 @@
+﻿package contactSys_web.servlet;
+
+import contactSys_web.dao.ContactDao;
+import contactSys_web.dao.impl.ContactDaoImpl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteContactServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//在火狐浏览器中以Get方式提交带参数的数据，会重复提交两次。
+		System.out.println("删除联系人");
+		//1.接收id
+		String id = request.getParameter("id");
+		
+		//2.调用dao删除联系人的方法
+		ContactDao dao = new ContactDaoImpl();
+		dao.deleteContact(id);
+		
+		//3.跳转到查询联系人的页面
+		response.sendRedirect(request.getContextPath()+"/ListContactServlet");
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
